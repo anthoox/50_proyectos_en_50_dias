9011f7c900eab8a85393f63d37d4ab0666fafdad
anthoox <kray.a4@gmail.com>
2024-08-17 13:50:52 +0200

    Optimización script

--- 07_split_landing_page/script.js
+++ 07_split_landing_page/script.js
@@ -1,37 +1,31 @@
 const gato = document.getElementById('gatos');
 const perro = document.getElementById('perros');
 gato.addEventListener('mouseover', ()=>{
-    agrandar(gato)
-    encoger(perro)
-    
+    agrandarEncoger(gato, perro)    
 })
 
 perro.addEventListener('mouseover', () => {
-    agrandar(perro)
-    encoger(gato)
+    agrandarEncoger(perro, gato)
 })
 
 perro.addEventListener('mouseleave', () => {
-    normalizar(perro)
-    normalizar(gato)
+    normalizar(perro, gato)
 })
 gato.addEventListener('mouseleave', () => {
-    normalizar(gato)
-    normalizar(perro)
-
+    normalizar(gato, perro)
 })
 
 
-function agrandar(elemento){
-    elemento.style.width = '75%';
-    elemento.style.zIndex = '10000';
+function agrandarEncoger(elemento1, elemento2){
+    elemento1.style.width = '75%';
+    elemento1.style.zIndex = '10000';
+
+    elemento2.style.width = '25%';
+    elemento2.style.zIndex = '10';
 }
 
-function encoger(elemento){
-    elemento.style.width = '25%';
-    elemento.style.zIndex = '10';
-}
+function normalizar(elemento1, elemento2){
+    elemento1.style.width = '50%';
+    elemento2.style.width = '50%';
 
-function normalizar(elemento){
-    elemento.style.width = '50%';
 }
