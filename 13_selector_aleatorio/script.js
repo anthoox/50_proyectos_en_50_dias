3a0d6905d5131c0977a66914c48b3c66a5e84828
anthoox <kray.a4@gmail.com>
2024-08-25 17:30:01 +0200

    Proyecto 13, Selector Aleatorio - comentando script

--- 13_selector_aleatorio/script.js
+++ 13_selector_aleatorio/script.js
@@ -39,37 +39,61 @@ textArea.addEventListener('keyup', (e)=>{
     });
 
 })
+
+/*
+Función que selecciona todos los elementos span y devuelve una etiqueta aleatoria.
+Para elegir le etiqueta, usamos Math.random * la longitud del array y se redonde para abajo
+*/
 function etiquetaAleatoria() {
     const etiquetas = document.querySelectorAll('span');
     return etiquetas[Math.floor(Math.random() * etiquetas.length)]
 }
 
-
+/*
+Función que inicia con un contador de hasta 30 veces para seleccionar una etiqueta aleatoria
+y darle un estilo para que se diferencie de las demás
+*/
 function selectorAleatorio(){
-    const veces = 30;
+    // Iniciamos el contador de hasta 30 veces
+    const contador = 30;
 
+    /* 
+    Almacenamos el intervalor en la constante saltos para más adelante acabarlo. El intervalo se reinicia a los 100 milisegundos
+    */
     const saltos = setInterval(()=>{
+        // Se selecciona un elemento span aleatorio
         const etiqueta = etiquetaAleatoria();
-
+        // Si el elemento es distinto a undefined
         if(etiqueta !== undefined){
+            // Se añade la clase seleccionado al elemento selecciado
             etiqueta.classList.add('seleccionado');
-
+            
+            /*
+            Usamos setTimeout para, tras 100 milisegundos, se elimine la clase seleccionado
+            y así dar el efecto de que se estan eligiendo aleatoriamente los elementos.
+            Finaliza quitando los estilos.
+            */
             setTimeout(() => {
                 etiqueta.classList.remove('seleccionado');
             }, 100)
         }
     }, 100)
 
+    // Usamos setTimeout para tras 3segundos se seleccione un elemento aleatorio
     setTimeout(() => {
+        // Se limpia la constante saltos para que termine
         clearInterval(saltos);
 
+        /* 
+        Se añade la clase seleccionado al elemento seleccionado. Se usa setTimeout para
+        que la función se ejecute despues de clearInterval.
+        */
         setTimeout(()=>{
             const etiqueta = etiquetaAleatoria();
             etiqueta.classList.add('seleccionado');
 
         },0)
-
-    }, veces * 100);
+    }, contador * 100);
 }
 
 
